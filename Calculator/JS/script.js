2613ebe0eb0a3ac197e290f1b8d7cde26dacddb0
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-06-25 00:53:32 +0530

    Add files via upload

---
+++ Calculator/JS/script.js
@@ -0,0 +1,37 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = " ";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener("click", (e) => {
+        try {
+            if (e.target.innerHTML == '=') {
+                string = eval(string);
+                input.value = string;
+            }
+            else if (e.target.innerHTML == "%") {
+                string = eval(string) / 100;
+                input.value = string;
+            }
+            else if (e.target.innerHTML == "AC") {
+                string = "";
+                input.value = string;
+            }
+            else if (e.target.innerHTML == "DEL") {
+                string = string.substring(0, string.length - 1);
+                input.value = string;
+            }
+            else if (string.length > 26) {
+                input.value = "INVALID LENGTH";
+            }
+            else {
+                string += e.target.innerHTML;
+                input.value = string;
+            }
+        }
+        catch (e) {
+            input.value = "SYNTAX ERROR";
+        }
+    })
+})
